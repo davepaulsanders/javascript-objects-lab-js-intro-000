89da395c3d038a0425c4f66a108a791d02605a20
David P Sanders <davepaulsanders@gmail.com>
2021-03-27 17:52:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
 var recipies = {}
-
- function destructivelypdateObjectWithKeyAndValue(recipes, key, value){
-   recipes[key] = value
-  return recipes
- }
-
-function deleteFromObjectByKey(recipes, key){
-  delete recipes[key]
-  return recipes
-}
